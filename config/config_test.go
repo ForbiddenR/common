323ca0629d021e001b926ba57b4fb6483a8d2be5
ForbiddenR <hostyouposition@outlook.com>
2025-04-23 13:24:44 +0800

    update to latest version

--- config/config_test.go
+++ config/config_test.go
@@ -20,6 +20,7 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/stretchr/testify/require"
 	"gopkg.in/yaml.v2"
 )
 
@@ -28,9 +29,11 @@ func TestJSONMarshalSecret(t *testing.T) {
 		S Secret
 	}
 	for _, tc := range []struct {
-		desc     string
-		data     tmp
-		expected string
+		desc          string
+		data          tmp
+		expected      string
+		marshalSecret bool
+		testYAML      bool
 	}{
 		{
 			desc: "inhabited",
@@ -40,30 +43,48 @@ func TestJSONMarshalSecret(t *testing.T) {
 			expected: "{\"S\":\"\\u003csecret\\u003e\"}",
 		},
 		{
+			desc:          "true value in JSON",
+			data:          tmp{"test"},
+			expected:      `{"S":"test"}`,
+			marshalSecret: true,
+		},
+		{
+			desc: "true value in YAML",
+			data: tmp{"test"},
+			expected: `s: test
+`,
+			marshalSecret: true,
+			testYAML:      true,
+		},
+		{
 			desc:     "empty",
 			data:     tmp{},
 			expected: "{\"S\":\"\"}",
 		},
 	} {
 		t.Run(tc.desc, func(t *testing.T) {
-			c, err := json.Marshal(tc.data)
-			if err != nil {
-				t.Fatal(err)
+			MarshalSecretValue = tc.marshalSecret
+
+			var marshalFN func(any) ([]byte, error)
+			if tc.testYAML {
+				marshalFN = yaml.Marshal
+			} else {
+				marshalFN = json.Marshal
 			}
-			if tc.expected != string(c) {
-				t.Fatalf("Secret not marshaled correctly, got '%s'", string(c))
-			}
+			c, err := marshalFN(tc.data)
+			require.NoError(t, err)
+			require.Equalf(t, tc.expected, string(c), "Secret not marshaled correctly, got '%s'", string(c))
 		})
 	}
 }
 
 func TestHeaderHTTPHeader(t *testing.T) {
 	testcases := map[string]struct {
-		header   Header
+		header   ProxyHeader
 		expected http.Header
 	}{
 		"basic": {
-			header: Header{
+			header: ProxyHeader{
 				"single": []Secret{"v1"},
 				"multi":  []Secret{"v1", "v2"},
 				"empty":  []Secret{},
@@ -85,9 +106,7 @@ func TestHeaderHTTPHeader(t *testing.T) {
 	for name, tc := range testcases {
 		t.Run(name, func(t *testing.T) {
 			actual := tc.header.HTTPHeader()
-			if !reflect.DeepEqual(actual, tc.expected) {
-				t.Fatalf("expecting: %#v, actual: %#v", tc.expected, actual)
-			}
+			require.Truef(t, reflect.DeepEqual(actual, tc.expected), "expecting: %#v, actual: %#v", tc.expected, actual)
 		})
 	}
 }
@@ -95,46 +114,42 @@ func TestHeaderHTTPHeader(t *testing.T) {
 func TestHeaderYamlUnmarshal(t *testing.T) {
 	testcases := map[string]struct {
 		input    string
-		expected Header
+		expected ProxyHeader
 	}{
 		"void": {
 			input: ``,
 		},
 		"simple": {
 			input:    "single:\n- a\n",
-			expected: Header{"single": []Secret{"a"}},
+			expected: ProxyHeader{"single": []Secret{"a"}},
 		},
 		"multi": {
 			input:    "multi:\n- a\n- b\n",
-			expected: Header{"multi": []Secret{"a", "b"}},
+			expected: ProxyHeader{"multi": []Secret{"a", "b"}},
 		},
 		"empty": {
 			input:    "{}",
-			expected: Header{},
+			expected: ProxyHeader{},
 		},
 		"empty value": {
 			input:    "empty:\n",
-			expected: Header{"empty": nil},
+			expected: ProxyHeader{"empty": nil},
 		},
 	}
 
 	for name, tc := range testcases {
 		t.Run(name, func(t *testing.T) {
-			var actual Header
+			var actual ProxyHeader
 			err := yaml.Unmarshal([]byte(tc.input), &actual)
-			if err != nil {
-				t.Fatalf("error unmarshaling %s: %s", tc.input, err)
-			}
-			if !reflect.DeepEqual(actual, tc.expected) {
-				t.Fatalf("expecting: %#v, actual: %#v", tc.expected, actual)
-			}
+			require.NoErrorf(t, err, "error unmarshaling %s: %s", tc.input, err)
+			require.Truef(t, reflect.DeepEqual(actual, tc.expected), "expecting: %#v, actual: %#v", tc.expected, actual)
 		})
 	}
 }
 
 func TestHeaderYamlMarshal(t *testing.T) {
 	testcases := map[string]struct {
-		input    Header
+		input    ProxyHeader
 		expected []byte
 	}{
 		"void": {
@@ -142,15 +157,15 @@ func TestHeaderYamlMarshal(t *testing.T) {
 			expected: []byte("{}\n"),
 		},
 		"simple": {
-			input:    Header{"single": []Secret{"a"}},
+			input:    ProxyHeader{"single": []Secret{"a"}},
 			expected: []byte("single:\n- <secret>\n"),
 		},
 		"multi": {
-			input:    Header{"multi": []Secret{"a", "b"}},
+			input:    ProxyHeader{"multi": []Secret{"a", "b"}},
 			expected: []byte("multi:\n- <secret>\n- <secret>\n"),
 		},
 		"empty": {
-			input:    Header{"empty": nil},
+			input:    ProxyHeader{"empty": nil},
 			expected: []byte("empty: []\n"),
 		},
 	}
@@ -158,12 +173,8 @@ func TestHeaderYamlMarshal(t *testing.T) {
 	for name, tc := range testcases {
 		t.Run(name, func(t *testing.T) {
 			actual, err := yaml.Marshal(tc.input)
-			if err != nil {
-				t.Fatalf("error unmarshaling %#v: %s", tc.input, err)
-			}
-			if !bytes.Equal(actual, tc.expected) {
-				t.Fatalf("expecting: %q, actual: %q", tc.expected, actual)
-			}
+			require.NoErrorf(t, err, "error unmarshaling %#v: %s", tc.input, err)
+			require.Truef(t, bytes.Equal(actual, tc.expected), "expecting: %q, actual: %q", tc.expected, actual)
 		})
 	}
 }
@@ -171,46 +182,42 @@ func TestHeaderYamlMarshal(t *testing.T) {
 func TestHeaderJsonUnmarshal(t *testing.T) {
 	testcases := map[string]struct {
 		input    string
-		expected Header
+		expected ProxyHeader
 	}{
 		"void": {
 			input: `null`,
 		},
 		"simple": {
 			input:    `{"single": ["a"]}`,
-			expected: Header{"single": []Secret{"a"}},
+			expected: ProxyHeader{"single": []Secret{"a"}},
 		},
 		"multi": {
 			input:    `{"multi": ["a", "b"]}`,
-			expected: Header{"multi": []Secret{"a", "b"}},
+			expected: ProxyHeader{"multi": []Secret{"a", "b"}},
 		},
 		"empty": {
 			input:    `{}`,
-			expected: Header{},
+			expected: ProxyHeader{},
 		},
 		"empty value": {
 			input:    `{"empty":null}`,
-			expected: Header{"empty": nil},
+			expected: ProxyHeader{"empty": nil},
 		},
 	}
 
 	for name, tc := range testcases {
 		t.Run(name, func(t *testing.T) {
-			var actual Header
+			var actual ProxyHeader
 			err := json.Unmarshal([]byte(tc.input), &actual)
-			if err != nil {
-				t.Fatalf("error unmarshaling %s: %s", tc.input, err)
-			}
-			if !reflect.DeepEqual(actual, tc.expected) {
-				t.Fatalf("expecting: %#v, actual: %#v", tc.expected, actual)
-			}
+			require.NoErrorf(t, err, "error unmarshaling %s: %s", tc.input, err)
+			require.Truef(t, reflect.DeepEqual(actual, tc.expected), "expecting: %#v, actual: %#v", tc.expected, actual)
 		})
 	}
 }
 
 func TestHeaderJsonMarshal(t *testing.T) {
 	testcases := map[string]struct {
-		input    Header
+		input    ProxyHeader
 		expected []byte
 	}{
 		"void": {
@@ -218,15 +225,15 @@ func TestHeaderJsonMarshal(t *testing.T) {
 			expected: []byte("null"),
 		},
 		"simple": {
-			input:    Header{"single": []Secret{"a"}},
+			input:    ProxyHeader{"single": []Secret{"a"}},
 			expected: []byte("{\"single\":[\"\\u003csecret\\u003e\"]}"),
 		},
 		"multi": {
-			input:    Header{"multi": []Secret{"a", "b"}},
+			input:    ProxyHeader{"multi": []Secret{"a", "b"}},
 			expected: []byte("{\"multi\":[\"\\u003csecret\\u003e\",\"\\u003csecret\\u003e\"]}"),
 		},
 		"empty": {
-			input:    Header{"empty": nil},
+			input:    ProxyHeader{"empty": nil},
 			expected: []byte(`{"empty":null}`),
 		},
 	}
@@ -234,12 +241,8 @@ func TestHeaderJsonMarshal(t *testing.T) {
 	for name, tc := range testcases {
 		t.Run(name, func(t *testing.T) {
 			actual, err := json.Marshal(tc.input)
-			if err != nil {
-				t.Fatalf("error marshaling %#v: %s", tc.input, err)
-			}
-			if !bytes.Equal(actual, tc.expected) {
-				t.Fatalf("expecting: %q, actual: %q", tc.expected, actual)
-			}
+			require.NoErrorf(t, err, "error marshaling %#v: %s", tc.input, err)
+			require.Truef(t, bytes.Equal(actual, tc.expected), "expecting: %q, actual: %q", tc.expected, actual)
 		})
 	}
 }
